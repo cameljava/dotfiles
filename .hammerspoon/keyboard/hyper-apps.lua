16b9006c8a62fd5258c7f4367d378586df6611ca
Kevin Lee <cameljava@gmail.com>
2019-11-23 20:07:15 +1100

    update my hyper apps

---
+++ .hammerspoon/keyboard/hyper-apps.lua
@@ -0,0 +1,14 @@
+-- keybindings for launching apps in Hyper Mode
+
+return {
+  { 'a', 'iTunes' },            -- "A" for "Apple Music"
+  { 'b', 'Google Chrome' },     -- "B" for "Browser"
+  { 'c', 'Slack' },             -- "C for "Chat"
+  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
+  { 'e', 'Visual Studio Code' },              -- "E" for "Editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 'm', 'MailMate' },       -- "M" for "mail"
+  { 's', 'Sourcetree' },             -- "S" for "Sourcetree"
+  { 't', 'iTerm' },             -- "T" for "Terminal"
+  { 'o', 'Microsoft Outlook' },             -- "O" for "Outlook"
+}
