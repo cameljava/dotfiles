91874fee28700b026d580c5e32e60446dbbab840
Kevin Lee <cameljava@gmail.com>
2020-04-13 20:47:33 +1000

    volume by ctrl+alt+cmd+up/down; dont forget superduper keyboard, jasonrudolph/keyboard

--- .hammerspoon/keyboard/hyper-apps.lua
+++ .hammerspoon/keyboard/hyper-apps.lua
@@ -5,10 +5,10 @@ return {
   { 'b', 'Google Chrome' },     -- "B" for "Browser"
   { 'c', 'Slack' },             -- "C for "Chat"
   { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
-  { 'e', 'Visual Studio Code' },              -- "E" for "Editor"
+  { 'e', 'Sublime Text' },              -- "E" for "Editor"
   { 'f', 'Finder' },            -- "F" for "Finder"
   { 'm', 'MailMate' },       -- "M" for "mail"
-  { 's', 'Sourcetree' },             -- "S" for "Sourcetree"
+  { 's', 'Fork' },             -- "S" for "Sourcetree"
   { 't', 'iTerm' },             -- "T" for "Terminal"
   { 'o', 'Microsoft Outlook' },             -- "O" for "Outlook"
 }
