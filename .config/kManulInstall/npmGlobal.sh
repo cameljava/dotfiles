03aed6d8b7d2717a6ff7ec8cacfa6cb7e5a221d6
Kevin Lee <kevlee@cochlear.com>
2021-10-14 22:04:44 +1100

    docu scripts need manually run for new machine set up

---
+++ .config/kManulInstall/npmGlobal.sh
@@ -0,0 +1,14 @@
+#! /usr/local/bin/bash
+
+# my work mac latest gnu bash
+
+# cli tools
+npm i -g graphqurl
+
+# neovim language servers
+npm i -g bash-language-server
+# check doc, need config and start server
+npm i -g graphql-language-service-cli
+npm i -g typescript-language-server
+npm i -g vim-language-server
+npm i -g yaml-language-server
