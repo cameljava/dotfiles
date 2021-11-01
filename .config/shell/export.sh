a349918d109d00318ccc9812c4065d5408ee0aa0
Kevin Lee <kevlee@cochlear.com>
2021-11-01 20:52:05 +1100

    add tmux config

--- .config/shell/export.sh
+++ .config/shell/export.sh
@@ -1,7 +1,8 @@
 # shellcheck shell=bash
 ################################# terminal config ####################
 export PS1='$ '
-export TERM=xterm-256color
+# export TERM=xterm-256color
+export TERM=screen-256color
 
 export PAGER="less"
 # export PAGER="most"
