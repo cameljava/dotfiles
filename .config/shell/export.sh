410ef571921314f162a2d497d42485c76f3d985e
Kevin Lee <cameljava@gmail.com>
2022-07-22 10:33:50 +1000

    update pyenv init in bashrc export

--- .config/shell/export.sh
+++ .config/shell/export.sh
@@ -49,8 +49,8 @@ export GEM_HOME="$HOME/.gem"
 
 # prompt show git status
 if [ -f "/opt/homebrew/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-  __GIT_PROMPT_DIR="/opt/homebrew/opt/bash-git-prompt/share"
-  source "/opt/homebrew/opt/bash-git-prompt/share/gitprompt.sh"
+	__GIT_PROMPT_DIR="/opt/homebrew/opt/bash-git-prompt/share"
+	source "/opt/homebrew/opt/bash-git-prompt/share/gitprompt.sh"
 fi
 
 export NVM_DIR="$HOME/.nvm"
@@ -58,9 +58,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 # pyenv for python
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH" # if `pyenv` is not already on PATH
 
-eval "$(pyenv init --path)"
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
