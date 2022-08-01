1a99d8352b448a0e8f735219ae37b1d29e3b54f6
Kevin Lee <cameljava@gmail.com>
2022-08-01 14:30:58 +1000

    add GV

--- .config/nvim-lua/plugin/packer_compiled.lua
+++ .config/nvim-lua/plugin/packer_compiled.lua
@@ -139,6 +139,11 @@ _G.packer_plugins = {
     path = "/Users/kevinlee/.local/share/nvim/site/pack/packer/start/gruvbox",
     url = "https://github.com/morhetz/gruvbox"
   },
+  ["gv.vim"] = {
+    loaded = true,
+    path = "/Users/kevinlee/.local/share/nvim/site/pack/packer/start/gv.vim",
+    url = "https://github.com/junegunn/gv.vim"
+  },
   ["impatient.nvim"] = {
     loaded = true,
     path = "/Users/kevinlee/.local/share/nvim/site/pack/packer/start/impatient.nvim",
