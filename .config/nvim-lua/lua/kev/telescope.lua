163833c8317b1d5bee02b9bb0f97d1485055eadc
Kevin Lee <cameljava@gmail.com>
2022-08-01 10:36:18 +1000

    add gitsign keymap

--- .config/nvim-lua/lua/kev/telescope.lua
+++ .config/nvim-lua/lua/kev/telescope.lua
@@ -1,142 +1,144 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 
-local actions = require "telescope.actions"
+local actions = require("telescope.actions")
 
-telescope.setup {
-  defaults = {
+telescope.setup({
+	defaults = {
 
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "smart" },
 
-    mappings = {
-      i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
+		mappings = {
+			i = {
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
 
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
 
-        ["<C-c>"] = actions.close,
+				["<C-c>"] = actions.close,
 
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
 
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
 
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
 
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
 
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-      },
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+			},
 
-      n = {
-        ["<esc>"] = actions.close,
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
+			n = {
+				["<esc>"] = actions.close,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
 
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
+				["j"] = actions.move_selection_next,
+				["k"] = actions.move_selection_previous,
+				["H"] = actions.move_to_top,
+				["M"] = actions.move_to_middle,
+				["L"] = actions.move_to_bottom,
 
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["gg"] = actions.move_to_top,
+				["G"] = actions.move_to_bottom,
 
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
 
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
 
-        ["?"] = actions.which_key,
-      },
-    },
-  },
-  extensions = {
-    command_palette = {
-      {"File",
-        { "entire selection (C-a)", ':call feedkeys("GVgg")' },
-        { "save current file (C-s)", ':w' },
-        { "save all files (C-A-s)", ':wa' },
-        { "quit (C-q)", ':qa' },
-        { "file browser (C-i)", ":lua require'telescope'.extensions.file_browser.file_browser()", 1 },
-        { "search word (A-w)", ":lua require('telescope.builtin').live_grep()", 1 },
-        { "git files (A-f)", ":lua require('telescope.builtin').git_files()", 1 },
-        { "files (C-f)",     ":lua require('telescope.builtin').find_files()", 1 },
-      },
-      {"Help",
-        { "tips", ":help tips" },
-        { "cheatsheet", ":help index" },
-        { "tutorial", ":help tutor" },
-        { "summary", ":help summary" },
-        { "quick reference", ":help quickref" },
-        { "search help(F1)", ":lua require('telescope.builtin').help_tags()", 1 },
-      },
-      {"Vim",
-        { "reload vimrc", ":source $MYVIMRC" },
-        { 'check health', ":checkhealth" },
-        { "jumps (Alt-j)", ":lua require('telescope.builtin').jumplist()" },
-        { "commands", ":lua require('telescope.builtin').commands()" },
-        { "command history", ":lua require('telescope.builtin').command_history()" },
-        { "registers (A-e)", ":lua require('telescope.builtin').registers()" },
-        { "colorshceme", ":lua require('telescope.builtin').colorscheme()", 1 },
-        { "vim options", ":lua require('telescope.builtin').vim_options()" },
-        { "keymaps", ":lua require('telescope.builtin').keymaps()" },
-        { "buffers", ":Telescope buffers" },
-        { "search history (C-h)", ":lua require('telescope.builtin').search_history()" },
-        { "paste mode", ':set paste!' },
-        { 'cursor line', ':set cursorline!' },
-        { 'cursor column', ':set cursorcolumn!' },
-        { "spell checker", ':set spell!' },
-        { "relative number", ':set relativenumber!' },
-        { "search highlighting (F12)", ':set hlsearch!' },
-      }
-    }
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  },
-}
+				["?"] = actions.which_key,
+			},
+		},
+	},
+	extensions = {
+		command_palette = {
+			{
+				"File",
+				{ "entire selection (C-a)", ':call feedkeys("GVgg")' },
+				{ "save current file (C-s)", ":w" },
+				{ "save all files (C-A-s)", ":wa" },
+				{ "quit (C-q)", ":qa" },
+				{ "file browser (C-i)", ":lua require'telescope'.extensions.file_browser.file_browser()", 1 },
+				{ "search word (A-w)", ":lua require('telescope.builtin').live_grep()", 1 },
+				{ "git files (A-f)", ":lua require('telescope.builtin').git_files()", 1 },
+				{ "files (C-f)", ":lua require('telescope.builtin').find_files()", 1 },
+			},
+			{
+				"Help",
+				{ "tips", ":help tips" },
+				{ "cheatsheet", ":help index" },
+				{ "tutorial", ":help tutor" },
+				{ "summary", ":help summary" },
+				{ "quick reference", ":help quickref" },
+				{ "search help(F1)", ":lua require('telescope.builtin').help_tags()", 1 },
+			},
+			{
+				"Vim",
+				{ "reload vimrc", ":source $MYVIMRC" },
+				{ "check health", ":checkhealth" },
+				{ "jumps (Alt-j)", ":lua require('telescope.builtin').jumplist()" },
+				{ "commands", ":lua require('telescope.builtin').commands()" },
+				{ "command history", ":lua require('telescope.builtin').command_history()" },
+				{ "registers (A-e)", ":lua require('telescope.builtin').registers()" },
+				{ "colorshceme", ":lua require('telescope.builtin').colorscheme()", 1 },
+				{ "vim options", ":lua require('telescope.builtin').vim_options()" },
+				{ "keymaps", ":lua require('telescope.builtin').keymaps()" },
+				{ "buffers", ":Telescope buffers" },
+				{ "search history (C-h)", ":lua require('telescope.builtin').search_history()" },
+				{ "paste mode", ":set paste!" },
+				{ "cursor line", ":set cursorline!" },
+				{ "cursor column", ":set cursorcolumn!" },
+				{ "spell checker", ":set spell!" },
+				{ "relative number", ":set relativenumber!" },
+				{ "search highlighting (F12)", ":set hlsearch!" },
+			},
+		},
+		-- Your extension configuration goes here:
+		-- extension_name = {
+		--   extension_config_key = value,
+		-- }
+		-- please take a look at the readme of the extension you want to configure
+	},
+})
 
 -- This will load fzy_native and have it override the default file sorter
-telescope.load_extension('fzf')
-
+telescope.load_extension("fzf")
 
 -- telescope extentions setting --
 
-telescope.load_extension('env')
-telescope.load_extension('command_palette')
-telescope.load_extension('node_modules')
-telescope.load_extension('repo')
+telescope.load_extension("env")
+telescope.load_extension("command_palette")
+telescope.load_extension("node_modules")
+telescope.load_extension("repo")
 
- -- telescope mapping
+-- telescope mapping
 local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
@@ -144,7 +146,7 @@ local keymap = vim.api.nvim_set_keymap
 keymap("n", "<leader>fp", "<cmd>Telescope command_palette<cr>", opts)
 
 keymap("n", "<leader>ff", "<cmd>Telescope git_files<cr>", opts)
--- keymap("n", "<leader>fd", "<cmd>Telescope fd<cr>", opts)
+keymap("n", "<leader>fd", "<cmd>Telescope fd<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
