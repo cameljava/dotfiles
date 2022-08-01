a6bb5690c545b15010cb35c8efec05ac20b20395
Kevin Lee <cameljava@gmail.com>
2022-08-01 15:40:46 +1000

    add gnu utils, update comment setting

--- .config/nvim-lua/lua/kev/treesitter.lua
+++ .config/nvim-lua/lua/kev/treesitter.lua
@@ -1,58 +1,87 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
-
 configs.setup {
-ensure_installed ={"javascript", "java", "html", "json", "yaml","rust","vim","toml","graphql","css", "dockerfile", "typescript", "python","bash", "lua", "jsdoc", "regex", "dockerfile", "http"},
-highlight = {
-  enable = true,
+  ensure_installed = {
+    "bash",
+    "comment",
+    "css",
+    "dockerfile",
+    "graphql",
+    "html",
+    "http",
+    "java",
+    "javascript",
+    "jsdoc",
+    "json",
+    "lua",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "prisma",
+    "python",
+    "query",
+    "regex",
+    "rust",
+    "sql",
+    "toml",
+    "typescript",
+    "vim",
+    "yaml",
   },
-incremental_selection = {
-  enable = true,
-  keymaps = {
-    init_selection = "gnn",
-    node_incremental = "gnr",
-    scope_incremental = "gnc",
-    node_decremental = "gnd",
-    },
-},
-textobjects = {
-  select = {
+  highlight = {
     enable = true,
-     -- Automatically jump forward to textobj, similar to targets.vim
-    lookahead = true,
-    keymaps ={
-    -- You can use the capture groups defined in textobjects.scm
-      ["af"] = "@function.outer",
-      ["if"] = "@function.inner",
-      ["ac"] = "@class.outer",
-      ["ic"] = "@class.inner",
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "gnr",
+      scope_incremental = "gnc",
+      node_decremental = "gnd",
     },
   },
-  move = {
-    enable = true,
-    set_jumps = true, -- whether to set jumps in the jumplist
-    goto_next_start = {
-      ["]m"] = "@function.outer",
-      ["]]"] = "@class.outer",
-      },
-    goto_next_end = {
-      ["]M"] = "@function.outer",
-      ["]["] = "@class.outer",
-      },
-    goto_previous_start = {
-      ["[m"] = "@function.outer",
-      ["[["] = "@class.outer",
-      },
-    goto_previous_end = {
-      ["[M"] = "@function.outer",
-      ["[]"] = "@class.outer",
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
       },
     },
-},
-refactor = {
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+  },
+  refactor = {
     highlight_definitions = {
       enable = true,
       -- Set to false if you have an `updatetime` of ~100.
@@ -60,14 +89,14 @@ refactor = {
     },
     highlight_current_scope = { enable = false },
     navigation = {
-        enable = true,
-        keymaps = {
-          goto_definition = "gnd",
-          list_definitions = "gnD",
-          list_definitions_toc = "gO",
-          goto_next_usage = "<a-*>",
-          goto_previous_usage = "<a-#>",
-        },
+      enable = true,
+      keymaps = {
+        goto_definition = "gnd",
+        list_definitions = "gnD",
+        list_definitions_toc = "gO",
+        goto_next_usage = "<a-*>",
+        goto_previous_usage = "<a-#>",
       },
+    },
   },
 }
