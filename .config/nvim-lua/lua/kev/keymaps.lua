5edf03b827138787d928e2bc8ffa600b910e919e
Kevin Lee <cameljava@gmail.com>
2022-08-01 14:20:51 +1000

    try out using ctrl+hjkl instead of leader

--- .config/nvim-lua/lua/kev/keymaps.lua
+++ .config/nvim-lua/lua/kev/keymaps.lua
@@ -48,10 +48,15 @@ keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Better window navigation
-keymap("n", "<leader>h", "<C-w>h", opts)
-keymap("n", "<leader>j", "<C-w>j", opts)
-keymap("n", "<leader>k", "<C-w>k", opts)
-keymap("n", "<leader>l", "<C-w>l", opts)
+-- keymap("n", "<leader>h", "<C-w>h", opts)
+-- keymap("n", "<leader>j", "<C-w>j", opts)
+-- keymap("n", "<leader>k", "<C-w>k", opts)
+-- keymap("n", "<leader>l", "<C-w>l", opts)
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 keymap("n", "<Up>", ":resize -2<CR>", opts)
