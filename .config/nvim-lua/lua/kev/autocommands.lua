159b911db96cb0fa90beb4bd9a0a4dc332c1abbb
Kevin Lee <cameljava@gmail.com>
2022-08-07 12:54:03 +1000

    add several autcmd inspired by learn-neovim-lua

--- .config/nvim-lua/lua/kev/autocommands.lua
+++ .config/nvim-lua/lua/kev/autocommands.lua
@@ -24,8 +24,45 @@ vim.cmd [[
     autocmd VimResized * tabdo wincmd = 
   augroup end
 
-  augroup _lsp
-    autocmd!
-    autocmd BufWritePre * lua vim.lsp.buf.formatting()
-  augroup end
+  " augroup _lsp
+  "   autocmd!
+  "   autocmd BufWritePre * lua vim.lsp.buf.formatting()
+  " augroup end
 ]]
+
+-- below copy from learn-neovim-lua
+-- https://github.com/nshen/learn-neovim-lua/blob/main/lua/autocmds.lua
+
+local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
+  clear = true,
+})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- autoformat while save
+autocmd("BufWritePre", {
+  group = myAutoGroup,
+  -- pattern = { "*.lua", "*.py", "*.sh" },
+  pattern = { "*.ts", "*.js", "*.md", "*.yml","*.yaml","*.toml", "*.json", "*.lua", "*.py", "*.sh" },
+  callback = vim.lsp.buf.formatting_sync,
+})
+
+-- Highlight on yank
+autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = myAutoGroup,
+  pattern = "*",
+})
+
+-- 用o换行不要延续注释
+autocmd("BufEnter", {
+  group = myAutoGroup,
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions
+      - "o" -- O and o, don't continue comments
+      + "r" -- But do continue when pressing enter.
+  end,
+})
