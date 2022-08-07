f8bbdef5ef5d8a7019b612430f6b5f571eada271
Kevin Lee <cameljava@gmail.com>
2022-08-07 11:46:23 +1000

    brew install luarocks; add lsp documentation hightlight

--- .config/nvim-lua/lua/kev/lsp/handlers.lua
+++ .config/nvim-lua/lua/kev/lsp/handlers.lua
@@ -37,23 +37,27 @@ M.setup = function()
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
-    width = 60,
   })
 
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
     border = "rounded",
-    width = 60,
   })
 end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  local status_ok, illuminate = pcall(require, "illuminate")
-  if not status_ok then
-    return
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+      false
+    )
   end
-  illuminate.on_attach(client)
-  -- end
 end
 
 local function lsp_keymaps(bufnr)
@@ -86,6 +90,7 @@ M.on_attach = function(client, bufnr)
   then
     client.resolved_capabilities.document_formatting = false
   end
+
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
