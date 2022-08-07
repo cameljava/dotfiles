fc53f1bc2bd7492089c2d01c081c763bab5825ca
Kevin Lee <cameljava@gmail.com>
2022-08-07 13:16:50 +1000

    set up my vimwiki

--- .config/nvim-lua/lua/kev/default.lua
+++ .config/nvim-lua/lua/kev/default.lua
@@ -1,3 +1,4 @@
+--  ==================  glow  ===================
 local status_ok, glow = pcall(require, "glow")
 if not status_ok then
   return
@@ -5,8 +6,10 @@ end
 
 glow.setup {}
 
+--  ==================  nvim-runscript  ===================
 require("nvim-runscript").setup {}
 
+--  ==================  colorizer  ===================
 require("colorizer").setup {
   "css",
   "javascript",
@@ -15,3 +18,19 @@ require("colorizer").setup {
     mode = "foreground",
   },
 }
+
+--  ==================  vimviki  ===================
+
+vim.cmd [[
+
+let wiki_1 = {}
+let wiki_1.path = '~/vimwiki/'
+let wiki_1.syntax = 'markdown'
+let wiki_1.ext = '.md'
+
+let g:vimwiki_global_ext = 0
+
+let g:vimwiki_list = [wiki_1]
+let g:vimwiki_ext2syntax = {'.md': 'markdown', '.markdown': 'markdown', '.mdown': 'markdown'}
+
+]]
