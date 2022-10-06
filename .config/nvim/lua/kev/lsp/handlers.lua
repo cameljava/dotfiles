75f52319a9afe29ad61f9ddf1aa4d258daad458f
Kevin Lee <cameljava@gmail.com>
2022-10-06 21:41:13 +1100

    clean up

--- .config/nvim/lua/kev/lsp/handlers.lua
+++ .config/nvim/lua/kev/lsp/handlers.lua
@@ -49,6 +49,7 @@ local function lsp_highlight_document(client)
   -- if client.server_capabilities.document_highlight then
   local status_ok, illuminate = pcall(require, "illuminate")
   if not status_ok then
+    print "error loading illuminate"
   end
   illuminate.on_attach(client)
 end
