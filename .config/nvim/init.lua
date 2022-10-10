2dda6bf186c27cdd164c275f5fc885c4b9a25a89
Kevin Lee <cameljava@gmail.com>
2022-10-10 21:50:19 +1100

    add opts for keymap, auto format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,11 @@
-require('kev.base')
-require('kev.highlights')
-require('kev.maps')
-  require('kev.plugins')
+require "kev.base"
+require "kev.highlights"
+require "kev.maps"
+require "kev.plugins"
 
 local has = vim.fn.has
 local is_mac = has "macunix"
 
 if is_mac then
-  require('kev.macos')
+  require "kev.macos"
 end
--- .config/nvim/lua/kev/maps.lua
+++ .config/nvim/lua/kev/maps.lua
@@ -1,34 +1,35 @@
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
-keymap.set('n', 'x', '"_x')
+keymap.set("n", "x", '"_x', opts)
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+keymap.set("n", "+", "<C-a>", opts)
+keymap.set("n", "-", "<C-x>", opts)
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+keymap.set("n", "dw", 'vb"_d', opts)
 
 -- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
+keymap.set("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
+keymap.set("n", "te", ":tabedit", opts)
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+keymap.set("n", "ss", ":split<Return><C-w>w", opts)
+keymap.set("n", "sv", ":vsplit<Return><C-w>w", opts)
 -- Move window
-keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'sh', '<C-w>h')
-keymap.set('', 'sk', '<C-w>k')
-keymap.set('', 'sj', '<C-w>j')
-keymap.set('', 'sl', '<C-w>l')
+keymap.set("n", "<Space>", "<C-w>w", opts)
+keymap.set("", "sh", "<C-w>h", opts)
+keymap.set("", "sk", "<C-w>k", opts)
+keymap.set("", "sj", "<C-w>j", opts)
+keymap.set("", "sl", "<C-w>l", opts)
 
 -- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+keymap.set("n", "<C-w><left>", "<C-w><", opts)
+keymap.set("n", "<C-w><right>", "<C-w>>", opts)
+keymap.set("n", "<C-w><up>", "<C-w>+", opts)
+keymap.set("n", "<C-w><down>", "<C-w>-", opts)
