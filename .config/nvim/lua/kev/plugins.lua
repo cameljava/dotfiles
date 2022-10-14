dd26597835d52a6bc68d31dfa970749f6fe31f98
Kevin Lee <cameljava@gmail.com>
2022-10-14 19:38:37 +1100

    lua-dev to neodev

--- .config/nvim/lua/kev/plugins.lua
+++ .config/nvim/lua/kev/plugins.lua
@@ -114,7 +114,7 @@ return packer.startup {
     use "neovim/nvim-lspconfig" -- enable LSP
     use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
-    use "folke/lua-dev.nvim"
+    use "folke/neodev.nvim"
     use "RRethy/vim-illuminate"
     -- snippets
     use "L3MON4D3/LuaSnip" --snippet engine
