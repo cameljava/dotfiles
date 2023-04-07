dc43e76cdf41378bfea7d54755782bccecb1bafd
Kevin Lee <cameljava@gmail.com>
2023-04-07 19:19:13 +1000

    switch to tokyonight

--- .config/nvim/after/plugin/tokyonight.rc.lua
+++ .config/nvim/after/plugin/tokyonight.rc.lua
@@ -23,6 +23,6 @@ vim.g.tokyonight_colors = {
   purple = "#6c71c4",
 }
 
--- vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
 -- vim.cmd [[colorscheme nordfox]]
-vim.cmd [[colorscheme everforest]]
+-- vim.cmd [[colorscheme everforest]]
