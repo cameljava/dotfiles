5423f6cafdd7a2defaccb07edfd8d89dbbca553c
Kevin Lee <cameljava@gmail.com>
2024-10-29 09:18:20 +1100

    add more mini modules

--- .config/nvim/lua/kev/plugins/mini.lua
+++ .config/nvim/lua/kev/plugins/mini.lua
@@ -32,7 +32,36 @@ return {
       statusline.section_location = function()
         return '%2l:%-2v'
       end
-
+      require('mini.diff').setup()
+      require('mini.test').setup()
+      require('mini.comment').setup()
+      require('mini.base16').setup({
+        palette = {
+          base00 = '#112641',
+          base01 = '#3a475e',
+          base02 = '#606b81',
+          base03 = '#8691a7',
+          base04 = '#d5dc81',
+          base05 = '#e2e98f',
+          base06 = '#eff69c',
+          base07 = '#fcffaa',
+          base08 = '#ffcfa0',
+          base09 = '#cc7e46',
+          base0A = '#46a436',
+          base0B = '#9ff895',
+          base0C = '#ca6ecf',
+          base0D = '#42f7ff',
+          base0E = '#ffc4ff',
+          base0F = '#00a5c5',
+        },
+        use_cterm = true,
+        plugins = {
+          default = false,
+          ['echasnovski/mini.nvim'] = true,
+          ['folke/lazy.nvim'] = true,
+          ['williamboman/mason.nvim'] = true,
+        },
+      })
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
