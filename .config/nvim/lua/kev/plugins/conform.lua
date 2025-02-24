418361d1801563f150bc6dbab98b9662d8e2ec9d
Kevin Lee <cameljava@gmail.com>
2025-02-25 10:37:15 +1100

    update conform

--- .config/nvim/lua/kev/plugins/conform.lua
+++ .config/nvim/lua/kev/plugins/conform.lua
@@ -37,15 +37,27 @@ return {
         }
       end,
       formatters_by_ft = {
+        sh = { 'shfmt' },
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
+        go = { 'goimports', 'gofmt' },
+        -- You can use a function here to determine the formatters dynamically
+        python = function(bufnr)
+          if require("conform").get_formatter_info("ruff_format", bufnr).available then
+            return { "ruff_format" }
+          else
+            return { "isort", "black" }
+          end
+        end,
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { 'prettier', 'prettierd' } },
+        typescript = { { 'prettier', 'prettierd' } },
         html = { { 'prettier', 'prettierd' } },
         css = { { 'prettier', 'prettierd' } },
+        markdown = { { 'prettier', 'prettierd' } },
+        java = { { 'google-java-format' } },
+        sql = { { 'sqlfluff', 'pg_format' } },
         json = {
           function()
             return {
@@ -74,11 +86,17 @@ return {
     },
     config = function()
       require('conform').setup {}
-      vim.api.nvim_create_user_command('Format', function()
-        require('conform').format { async = true, silent = false, lsp_fallback = true }
-      end, {
-        desc = 'format-command',
-      })
+      vim.api.nvim_create_user_command("Format", function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+          }
+        end
+        require("conform").format({ async = true, lsp_format = "fallback", range = range })
+      end, { range = true })
       vim.api.nvim_create_user_command('FormatDisable', function(args)
         if args.bang then
           -- FormatDisable! will disable formatting just for this buffer
