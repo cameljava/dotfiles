d0560d7951c67012ae983ed8baf2f23953cc4259
Kevin Lee <cameljava@gmail.com>
2025-05-17 11:00:53 +1000

    dd tsserver lsp config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,6 +18,11 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- Any lazyvim.plugins.extras.* (official extras)
+    -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+    -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
